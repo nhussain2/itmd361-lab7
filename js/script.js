bd5bd35966aec9f42bace5684d0803b08fb4c85f
Naveed Hussain <nhussain2@hawk.iit.edu>
2019-11-16 15:49:10 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 function init(){
 //add your javascript between these two lines of code
- 
+	alert('Hello Naveed');
+}
+
+var d2 = document.getElementById('entrybutton');
+d2.onclick = init;
 
 
-
-
-
-
-
-window.addEventListener('load', init);
