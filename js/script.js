00a0fd1648727bcaba4fe0700435ff48ed6a50f2
Naveed Hussain <nhussain2@hawk.iit.edu>
2019-11-18 16:01:56 -0600

    js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function init(){
 //add your javascript between these two lines of code
-	alert('hello')
+	document.getElementById("entrybutton").addEventListener("click", myFunction);
+
+function myFunction(){
+    var text = document.getElementById('entryinput').value
+    document.getElementById('textoutput').innerHTML = text;
+    alert('Naveed Hussain:' + text);
+}};
 
 window.addEventListener('load', init);
 
